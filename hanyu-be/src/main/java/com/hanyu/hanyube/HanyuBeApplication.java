6abf2360b6d80d7aa171cf213800b23bb3242926
khoahd7621 <hoangdangkhoa7621@gmail.com>
2023-05-14 23:05:15 +0700

    Init backend project

---
+++ hanyu-be/src/main/java/com/hanyu/hanyube/HanyuBeApplication.java
@@ -0,0 +1,13 @@
+package com.hanyu.hanyube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanyuBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanyuBeApplication.class, args);
+	}
+
+}
