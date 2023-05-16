f5e6ddc77f68bb46165e1007b86d80cc7089eea1
khoahd7621 <hoangdangkhoa7621@gmail.com>
2023-05-16 16:27:22 +0700

    Setup ci/cd for hanyu-be

--- hanyu-be/src/test/java/com/hanyu/hanyube/HanyuBeApplicationTests.java
+++ hanyu-be/src/test/java/com/hanyu/hanyube/HanyuBeApplicationTests.java
@@ -1,13 +1,11 @@
 package com.hanyu.hanyube;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class HanyuBeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
