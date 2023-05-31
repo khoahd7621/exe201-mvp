1455a8f223d2cd97bd46ee3ab99aa7bfbcd5f90c
Thái Thành Phát <87026966+ttphats@users.noreply.github.com>
2023-05-31 20:34:54 +0700

    implement translate page (#6)
    * implement translate page
    
    * move function to useEffect
    
    * change label to Vietnamese
    
    * Change tabs from mui-lab to mui-core
    
    * Add @mui/system
    
    ---------
    
    Co-authored-by: khoahd7621 <hoangdangkhoa7621@gmail.com>

---
+++ hanyu-fe/src/modules/translate/components/index.ts
@@ -0,0 +1,3 @@
+import TranslateBox from "./TranslateBox";
+
+export { TranslateBox };
--- hanyu-fe/src/pages/TranslatePage.tsx
+++ hanyu-fe/src/pages/TranslatePage.tsx
@@ -1,11 +1,10 @@
-import { Container, Typography } from "@mui/material";
+import { Container } from "@mui/material";
+import { TranslateBox } from "~/modules/translate/components";
 
 export default function TranslatePage() {
   return (
     <Container maxWidth={false} sx={{ margin: "2rem 0" }}>
-      <Typography variant="h4" sx={{ textAlign: "center" }}>
-        This feature is comming soon.
-      </Typography>
+      <TranslateBox />
     </Container>
   );
 }
