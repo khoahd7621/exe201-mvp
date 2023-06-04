74695d3c8adbf1611b91eb08190b95f26cdf5793
namth1606 <namthse150921@gmail.com>
2023-06-04 21:40:54 +0700

    add logback

---
+++ hanyu-be/src/main/java/com/hanyu/hanyube/config/RequestLoggingFilterConfig.java
@@ -0,0 +1,19 @@
+package com.hanyu.hanyube.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter commonsRequestLoggingFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(false);
+        filter.setIncludeHeaders(false);
+        filter.setBeforeMessagePrefix("Calling  : ");
+        return filter;
+    }
+}
