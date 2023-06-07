62811df47d8f72b13884ef6b496e0ad624c48756
namth1606 <namthse150921@gmail.com>
2023-06-07 23:56:32 +0700

    hot fix update word note

--- hanyu-be/src/main/java/com/hanyu/hanyube/config/ModelMapperConfig.java
+++ hanyu-be/src/main/java/com/hanyu/hanyube/config/ModelMapperConfig.java
@@ -1,6 +1,9 @@
 package com.hanyu.hanyube.config;
 
+import com.hanyu.hanyube.domain.dto.wordnote.UpdateWordNoteRequest;
+import com.hanyu.hanyube.service.entities.WordNoteEntity;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +13,15 @@ public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration()
+        modelMapper.addMappings(new PropertyMap<UpdateWordNoteRequest, WordNoteEntity>() {
+                    @Override
+                    protected void configure() {
+                        skip(destination.getId());
+                    }
+                });
+        modelMapper
+                .getConfiguration()
+                .setFieldMatchingEnabled(true)
                 .setAmbiguityIgnored(true)
                 .setSkipNullEnabled(true)
                 .setMatchingStrategy(MatchingStrategies.STANDARD);
